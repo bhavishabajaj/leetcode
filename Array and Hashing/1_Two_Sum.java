ba04184963cfca7fb595aa1e28ce38152aeffb3b
Bhavisha <bhavishabajaj20@gmail.com>
2026-07-25 22:57:03 +0530

    Day4

---
+++ Array and Hashing/1_Two_Sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer , Integer> map = new HashMap<>();
+        for(int i= 0 ; i<nums.length ; i++){
+            int find = target - nums[i];
+            if(!map.containsKey(find)){
+                map.put(nums[i] , i);
+            }
+            else{
+            return new int[]{ i , map.get(find)};
+            }
+        }
+        return new int[]{};
+    }
+}
